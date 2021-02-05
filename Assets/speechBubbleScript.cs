d16f455e717a1293af980349e628a9ab4270f5dd
DaFDango <Ludvig.schyberg@hotmail.com>
2021-02-05 12:29:26 +0100

    UI v1 start
    Added the textbox

---
+++ Assets/speechBubbleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class speechBubbleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+            
+    }
+}
